341768a19cbdecd289685a2e3ed86cea20866854
scoleray <scoleray@student.21-school.ru>
2023-11-28 16:57:40 +0300

    message.cc

---
+++ message.cc
@@ -0,0 +1,15 @@
+﻿#include "message.h"
+
+std::string Message::getMessage() const { return _message; }
+
+void Message::setMessage(const std::string& message) { _message = message; }
+
+std::string Message::getSender() const { return _sender; }
+
+void Message::setSender(const std::string& sender) { _sender = sender; }
+
+std::string Message::getRecipient() const { return _recipient; }
+
+void Message::setRecipient(const std::string& recipient) {
+  _recipient = recipient;
+}
